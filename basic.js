f1fb043380fff5271a3b75105c9231a3e6e8c65b
ChaosOp <tp6ru8bj6@gmail.com>
2022-07-31 01:56:10 +0800

    init commit

---
+++ basic.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} selector
+ * @param {HTMLElement} ref
+ * @returns {HTMLElement[]}
+ */
+window.get = (selector, ref = document) => {
+    return Array.from(ref.querySelectorAll(selector)).map((e) => {
+        e.get = (selector) => window.get(selector, e);
+        return e;
+    });
+}
+
+
+/**
+ * @param {string} tagname
+ * @param {object} properties
+ * @returns {HTMLElement}
+ */
+window.new_node = (tagname, properties = {}) => {
+    let property_modifier = (obj, properties) => {
+        for (let key of Object.keys(properties)) {
+            (
+                (typeof (obj[key]) === 'object') ?
+                    (property_modifier(obj[key], properties[key]))
+                    :
+                    (obj[key] = properties[key])
+            );
+        }
+
+        return obj;
+    };
+    return property_modifier(document.createElement(tagname), properties);
+}
---
+++ main.js
@@ -0,0 +1,135 @@
+const width_list = [28, 44, 28];
+
+const default_pos = {
+    "#refresh_inv": [1080, 10],
+    "#refresh_map": [5, 65],
+    "#refresh_chat": [1570, 70]
+};
+
+const text_list = {
+    "refresh": {
+        "zh": "重整",
+        "en": "Refresh"
+    },
+    "inv": {
+        "zh": "背包",
+        "en": "Inv."
+    },
+    "map": {
+        "zh": "主畫面",
+        "en": "Main"
+    },
+    "chat": {
+        "zh": "聊天室",
+        "en": "Chat"
+    }
+};
+
+const lang = localStorage.lang ?? "zh";
+
+["inv", "map", "chat"].forEach((route, i) => {
+    let frame = window.new_node("iframe", {
+        id: `${route}_window`,
+        frameBorder: "0",
+        src: `https://cybercodeonline.com/tabs/${route}`,
+        style: {
+            "width": `${width_list[i]}%`,
+        }
+    });
+
+    window.get("#wrap")[0].appendChild(frame);
+
+    add_refresh(route, frame);
+});
+
+
+function init_drag(selector) {
+
+    let dragElement = window.get(selector)[0];
+    if (dragElement.initedDrag) return;
+
+    let eventList = (window.ontouchstart === undefined) ?
+        (['mousedown', 'mousemove', 'mouseup'])
+        :
+        (['touchstart', 'touchmove', 'touchend']);
+
+    let [startEvt, moveEvt, endEvt] = eventList;
+
+
+    if (localStorage.tempPos !== undefined) setElementPos(selector, ...(JSON.parse(localStorage.tempPos)?.[selector] ?? [0, 0]));
+
+    dragElement.style.cursor = 'move';
+    dragElement.initedDrag = true;
+
+    dragElement.addEventListener(startEvt, (dragEvent) => {
+
+        dragElement.drag = false;
+
+        dragEvent.preventDefault();
+
+        let startPos = getEventPos(dragEvent);
+
+        let distance = ["Left", "Top"].map((type, i) => startPos[i] - dragElement[`offset${type}`]);
+
+        let moveHandler = (event) => {
+
+            dragElement.drag = true;
+
+            const border = document.documentElement.clientWidth - dragElement.offsetWidth;
+            let pos = getEventPos(event).map((pos, i) => Math.min(Math.max(pos - distance[i], 0), border));
+
+            setElementPos(selector, ...pos);
+        }
+
+        let endHandler = () => {
+            dragElement.removeEventListener(moveEvt, moveHandler);
+            dragElement.removeEventListener(endEvt, endHandler);
+        }
+
+        dragElement.addEventListener(moveEvt, moveHandler);
+        dragElement.addEventListener(endEvt, endHandler);
+    });
+
+    function getEventPos(event) {
+        return ["clientX", "clientY"].map((type) => event.touches?.[0][type] ?? event[type]);
+    }
+
+    function setElementPos(selector, left = 0, top = 0) {
+        let element = window.get(selector)[0];
+        let pos = JSON.parse(localStorage.tempPos ?? "{}");
+
+        if (left || top) {
+            pos[selector] = [left, top];
+        }
+
+        if (!pos[selector]) {
+            pos[selector] = default_pos[selector];
+        }
+
+        element.style.left = `${pos[selector][0]}px`;
+        element.style.top = `${pos[selector][1]}px`;
+
+
+        localStorage.tempPos = JSON.stringify(pos);
+    }
+}
+
+function add_refresh(id, window_ref) {
+
+
+    let refresh = window.new_node("a", {
+        id: `refresh_${id}`,
+        innerText: ` ${text_list.refresh[lang]} ${text_list[id][lang]} `
+    });
+
+    refresh.addEventListener("mouseup", () => {
+        if (refresh.drag) return;
+        window_ref.src = window_ref.src;
+    });
+
+
+    setTimeout(() => {
+        window.get("body")[0].appendChild(refresh);
+        init_drag(`#refresh_${id}`);
+    }, 2000);
+}
