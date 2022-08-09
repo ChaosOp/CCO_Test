2ddd87424465947d29f75c3656477cf2b504e4f6
ChaosOp <tp6ru8bj6@gmail.com>
2022-08-09 09:54:40 +0800

    update main.js add localStorage Proxy rearrange layout add switch to inv & chat

--- main.js
+++ main.js
@@ -1,33 +1,74 @@
-const widthList = [27, 30, 27];
+const widthList = [29, 100, 28];
+
 
 const defaultPos = {
-    "#refreshInv": [550, 10],
-    "#refreshMap": [1255, 5],
-    "#refreshChat": [1600, 60]
+    "#refreshInv": [510, 10],
+    "#refreshMap": [1100, 5],
+    "#refreshChat": [1600, 60],
+    "#switchInv": [1180, 5],
+    "#switchChat": [1240, 5]
 };
 
 const iconList = {
-    "Inv": "https://i.imgur.com/WJA22Rn.png",
-    "Map": "https://i.imgur.com/k8KdSVE.png",
-    "Chat": "https://i.imgur.com/VvRco9j.png"
+    "refresh": {
+        "Inv": "https://i.imgur.com/WJA22Rn.png",
+        "Map": "https://i.imgur.com/k8KdSVE.png",
+        "Chat": "https://i.imgur.com/VvRco9j.png"
+    },
+    "switch": {
+        "Inv": "https://i.imgur.com/6EiLQbu.png",
+        "Chat": "https://i.imgur.com/mkrQJQB.png"
+    }
 };
 
+(() => {
 
-["Inv", "Map", "Chat"].forEach((route, i) => {
-    let frame = window.newNode("iframe", {
-        id: `${route}Window`,
-        frameBorder: "0",
-        src: `https://cybercodeonline.com/tabs/${route}`,
-        style: {
-            "width": `${widthList[i]}%`,
-            "margin": `0px ${i == 1 ? "8%" : ""}`
-        }
+    window.ls = ls_proxy(localStorage);
+
+    ["switchStatus", "tempPos"].forEach((key) => {
+        if (!ls[key]) ls[key] = {};
     });
 
-    window.get("#wrap")[0].appendChild(frame);
 
-    addRefresh(route, frame);
-});
+    ["Inv", "Map", "Chat"].forEach((route, i) => {
+        const notMap = route !== "Map";
+
+        let frame = window.newNode("iframe", {
+            id: `${route}Window`,
+            frameBorder: "0",
+            src: `https://cybercodeonline.com/tabs/${route}`,
+            style: {
+                "width": `${widthList[i]}%`,
+                "position": notMap ? "absolute" : "",
+                "z-index": notMap ? 500 : 0,
+                "left": `${i * (100 - widthList[i]) / 2}%`
+            }
+        });
+
+        window.get("#wrap")[0].appendChild(frame);
+
+        addDragButton("refresh", route, (refresh) => {
+            if (refresh.drag) return;
+            frame.src = frame.src;
+        });
+
+        if (notMap) {
+            const setSwitchStatus = (OnOffSwitch, status) => {
+                OnOffSwitch.style.backgroundColor = (+status) ? "#bdffc8" : "#fc5688";
+                frame.style.display = (+status) ? "" : "none";
+                ls.switchStatus[OnOffSwitch.id] = status;
+            };
+
+            const OnOffSwitch = addDragButton("switch", route, (OnOffSwitch) => {
+                if (OnOffSwitch.drag) return;
+                setSwitchStatus(OnOffSwitch, +!ls.switchStatus[OnOffSwitch.id]);
+            });
+
+            setSwitchStatus(OnOffSwitch, ls.switchStatus[OnOffSwitch.id] ?? 1);
+        }
+
+    });
+})();
 
 /**
  * @param {String} selector selector of the element to be inited
@@ -45,7 +86,7 @@ function initDrag(selector) {
     let [startEvt, moveEvt, endEvt] = eventList;
 
 
-    setElementPos(selector, ...(JSON.parse(localStorage.tempPos ? localStorage.tempPos : "{}")?.[selector] ?? [0, 0]));
+    setElementPos(selector, ...(ls.tempPos[selector] ?? [0, 0]));
 
     dragElement.style.cursor = 'move';
     dragElement.initedDrag = true;
@@ -95,7 +136,7 @@ function initDrag(selector) {
      */
     function setElementPos(selector, left = 0, top = 0) {
         let element = window.get(selector)[0];
-        let pos = JSON.parse(localStorage.tempPos ? localStorage.tempPos : "{}");
+        let pos = ls.tempPos;
 
         if (left || top) {
             pos[selector] = [left, top];
@@ -113,35 +154,76 @@ function initDrag(selector) {
         element.style.left = `${getPercentBorder(pos[selector][0], window.innerWidth, widthLimit)}%`;
         element.style.top = `${getPercentBorder(pos[selector][1], window.innerHeight, heightLimit)}%`;
 
-        localStorage.tempPos = JSON.stringify(pos);
+        ls.tempPos = pos;
     }
 }
 
 
 /**
- * @param {String} id id of IFrame
- * @param {HTMLIFrameElement} windowRef selected IFrame 
+ * @param {String} type type of button
+ * @param {String} id id of IFrame 
+ * @param {Function} event callback when mouseup
+ * @returns {HTMLElement} the button added
  */
-function addRefresh(id, windowRef) {
+function addDragButton(type, id, event) {
 
-
-    const refresh = window.newNode("a", {
-        id: `refresh${id}`
+    const dragButton = window.newNode("a", {
+        id: `${type}${id}`
     });
 
     const icon = window.newNode("img", {
-        src: iconList[id]
+        src: iconList[type][id]
     });
-    refresh.appendChild(icon);
+    dragButton.appendChild(icon);
 
-    refresh.addEventListener("mouseup", () => {
-        if (refresh.drag) return;
-        windowRef.src = windowRef.src;
-    });
-
+    dragButton.addEventListener("mouseup", () => event(dragButton));
 
     setTimeout(() => {
-        window.get("body")[0].appendChild(refresh);
-        initDrag(`#refresh${id}`);
-    }, 2000);
+        window.get("body")[0].appendChild(dragButton);
+        initDrag(`#${type}${id}`);
+    }, 1500);
+
+    return dragButton;
+}
+
+function ls_proxy(obj, keys = []) {
+    return new Proxy(obj, { get, set });
+
+    function get(obj, p, is_proxy = true) {
+
+        if (!p.toString()) return obj;
+
+        let result = obj;
+        for (let k of p.toString().split(',')) {
+            if (k in result) result = result[k];
+            else break;
+        }
+        if (result === obj) return undefined;
+
+        try {
+            result = JSON.parse(result);
+        }
+        catch { }
+
+
+        if (!Array.isArray(result) && typeof (result) === 'object' && is_proxy) {
+            result = ls_proxy(result, keys.concat(p));
+        }
+
+        return result;
+    }
+
+    function set(obj, p, val) {
+
+        if (keys.length !== 0) {
+            let original_key = keys.shift();
+            let pre_obj = get(localStorage, original_key, false);
+            get(pre_obj, keys, false)[p] = val;
+
+            localStorage[original_key] = JSON.stringify(pre_obj);
+        }
+
+        obj[p] = JSON.stringify(val);
+    }
+
 }
